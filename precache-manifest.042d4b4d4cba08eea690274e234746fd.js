6a8cced94ffb441dba9daf1ab59233b0ee3159be
Labib Hasan <labib.hasan7102@gmail.com>
2020-08-13 12:03:40 +0600

    Updates

---
+++ precache-manifest.042d4b4d4cba08eea690274e234746fd.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e04d19e50a70ff4bc34eeec422d417",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "c746bcf231bb8fd8e8e2",
+    "url": "/reactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "d0a7237abbb5226efff2",
+    "url": "/reactwebsite/static/css/main.8d691836.chunk.css"
+  },
+  {
+    "revision": "c746bcf231bb8fd8e8e2",
+    "url": "/reactwebsite/static/js/2.a333ee84.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.a333ee84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a7237abbb5226efff2",
+    "url": "/reactwebsite/static/js/main.18c57e21.chunk.js"
+  },
+  {
+    "revision": "bcb148971045ac2f2f2f",
+    "url": "/reactwebsite/static/js/runtime-main.fe7cde09.js"
+  },
+  {
+    "revision": "b5e329a4e454723c3302cdea02ca8350",
+    "url": "/reactwebsite/static/media/affiliate.b5e329a4.jpg"
+  },
+  {
+    "revision": "2be4b4c7a5c0ff8c90d061d93e771f80",
+    "url": "/reactwebsite/static/media/code.2be4b4c7.svg"
+  },
+  {
+    "revision": "81ba3db9d785087e5399b6fb9efe406d",
+    "url": "/reactwebsite/static/media/dataentry.81ba3db9.jpg"
+  },
+  {
+    "revision": "842ab334511025ee7c4e1f7e8862ed8b",
+    "url": "/reactwebsite/static/media/digital.842ab334.jpg"
+  },
+  {
+    "revision": "d670d9997fdf9b7dc997c957682c59a7",
+    "url": "/reactwebsite/static/media/freelance.d670d999.jpg"
+  },
+  {
+    "revision": "b8ff8312a75ba60ff678679c2414555c",
+    "url": "/reactwebsite/static/media/game.b8ff8312.jpg"
+  },
+  {
+    "revision": "e4b563283e4923d2ed9163b29d09515f",
+    "url": "/reactwebsite/static/media/graphics.e4b56328.jpg"
+  },
+  {
+    "revision": "fc0973910a4b7c8b4eb8d0631d9535b8",
+    "url": "/reactwebsite/static/media/iphone.fc097391.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactwebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f6a617ac900243cea748c80908429f18",
+    "url": "/reactwebsite/static/media/marketting.f6a617ac.jpg"
+  },
+  {
+    "revision": "24f43957bb01907a54738869db565ca1",
+    "url": "/reactwebsite/static/media/photography.24f43957.jpg"
+  },
+  {
+    "revision": "145ddaad9b4f7fa65a4ae3eed81eaac7",
+    "url": "/reactwebsite/static/media/seo.145ddaad.jpg"
+  },
+  {
+    "revision": "9927a74066d57de02e80884a9136e2df",
+    "url": "/reactwebsite/static/media/team-management.9927a740.svg"
+  },
+  {
+    "revision": "b0b9d5da2e54e790de87b2f4ace748f4",
+    "url": "/reactwebsite/static/media/undraw_contact_us_15o2.b0b9d5da.svg"
+  },
+  {
+    "revision": "7e15cd05710b1db55912dfb8acd27907",
+    "url": "/reactwebsite/static/media/undraw_programmer_imem.7e15cd05.svg"
+  },
+  {
+    "revision": "29a9e36b6eecd9067d33cb0c8e51bd12",
+    "url": "/reactwebsite/static/media/undraw_web_developer_p3e5.29a9e36b.svg"
+  },
+  {
+    "revision": "c8e7a31ac802eb1c2906f502452b9de8",
+    "url": "/reactwebsite/static/media/webdev.c8e7a31a.jpg"
+  },
+  {
+    "revision": "2f7526fcc8404185b7daf1497c40f9cf",
+    "url": "/reactwebsite/static/media/youtube.2f7526fc.jpg"
+  }
+]);
